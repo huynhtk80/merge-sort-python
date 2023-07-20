bd05d2d8f9b14abb41dedcb8866c85beaa6c90ba
Timothy H <huynh.tk80@gmail.com>
2023-07-20 15:41:32 -0600

    test: sort 1-10

--- test.py
+++ test.py
@@ -18,5 +18,12 @@ class testMergeSort(unittest.TestCase):
         merge_sort(array)
         self.assertEqual(array, [5])
 
+    def test_sort_10digit(self):
+        '''test that sorts 1 to 10'''
+
+        array = [5,10,6,1,8,2,4,3,9,7]
+        merge_sort(array)
+        self.assertEqual(array, [1,2,3,4,5,6,7,8,9,10])
+
 if __name__ == '__main__':
     unittest.main()
